73dca9e4ed8bbee44bab1a0eeadbf7a2c37e9645
CurlyWendy <bubblegumsharkie@gmail.com>
2024-05-16 12:30:45 +0500

    Класс Patients (Редакция)

--- Users/models/patient.py
+++ Users/models/patient.py
@@ -2,9 +2,15 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Patient(AbstractUser):
-    user = models.OneToOneField(AbstractUser, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=255)
-    benefits = models.TextField()
+    user = models(AbstractUser, on_delete=models.CASCADE)
+    benefits = models.ForeignKey(
+        Benefits,
+        on_delete=models.CASCADE,
+        related_name='benefits'
+    )
+    age = models.PositiveIntegerField(default=NULL)
+    snils = models.CharField(max_length=30)
+    policy_number = models.CharField(max_length=30)
 
     def __str__(self):
         return self.full_name
